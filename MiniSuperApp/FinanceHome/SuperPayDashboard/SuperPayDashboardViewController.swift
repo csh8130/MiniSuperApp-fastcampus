edd0c8c977330efddcf97c285c80e587d5e5e633
csh8130 <csh8130@naver.com>
2021-11-21 01:47:16 +0900

    잔고 화면 헤더영역 생성

--- MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
@@ -17,4 +17,60 @@ protocol SuperPayDashboardPresentableListener: AnyObject {
 final class SuperPayDashboardViewController: UIViewController, SuperPayDashboardPresentable, SuperPayDashboardViewControllable {
 
     weak var listener: SuperPayDashboardPresentableListener?
+    
+    private let headerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "슈퍼페이 잔고"
+        return label
+    }()
+    
+    private lazy var topupButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("충전하기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(topupButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.addSubview(headerStackView)
+        
+        headerStackView.addArrangedSubview(titleLabel)
+        headerStackView.addArrangedSubview(topupButton)
+        
+        NSLayoutConstraint.activate([
+            headerStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    @objc
+    private func topupButtonDidTap() {
+        
+    }
 }
