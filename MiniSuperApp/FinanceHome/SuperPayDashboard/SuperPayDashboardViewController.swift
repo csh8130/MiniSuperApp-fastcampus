d41b7f3eae5ed04109fad51e8ad859ce348e2832
csh8130 <csh8130@naver.com>
2021-11-20 14:09:21 +0900

    잔고 리블렛 생성

---
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SuperPayDashboardViewController.swift
+//  MiniSuperApp
+//
+//  Created by korit on 2021/11/20.
+//
+
+import ModernRIBs
+import UIKit
+
+protocol SuperPayDashboardPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class SuperPayDashboardViewController: UIViewController, SuperPayDashboardPresentable, SuperPayDashboardViewControllable {
+
+    weak var listener: SuperPayDashboardPresentableListener?
+}
