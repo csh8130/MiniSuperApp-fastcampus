62e9da2e8667bc3308ab3da22b928cb69db9c80e
csh8130 <csh8130@naver.com>
2021-11-21 13:54:35 +0900

    잔액 stream으로부터 subscribe해서 잔액 label update

--- MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardInteractor.swift
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardInteractor.swift
@@ -6,6 +6,7 @@
 //
 
 import ModernRIBs
+import Combine
 
 protocol SuperPayDashboardRouting: ViewableRouting {
     // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
@@ -13,7 +14,8 @@ protocol SuperPayDashboardRouting: ViewableRouting {
 
 protocol SuperPayDashboardPresentable: Presentable {
     var listener: SuperPayDashboardPresentableListener? { get set }
-    // TODO: Declare methods the interactor can invoke the presenter to present data.
+    
+    func updateBalance(_ balance: String)
 }
 
 protocol SuperPayDashboardListener: AnyObject {
@@ -31,18 +33,24 @@ final class SuperPayDashboardInteractor: PresentableInteractor<SuperPayDashboard
 
     private let dependency: SuperPayDashboardInteractorDependency
     
+    private var cancellables: Set<AnyCancellable>
+    
     init(
         presenter: SuperPayDashboardPresentable,
         dependency: SuperPayDashboardInteractorDependency
     ) {
         self.dependency = dependency
+        self.cancellables = .init()
         super.init(presenter: presenter)
         presenter.listener = self
     }
 
     override func didBecomeActive() {
         super.didBecomeActive()
-        // TODO: Implement business logic here.
+        
+        dependency.balance.sink { [weak self] balance in
+            self?.presenter.updateBalance(String(balance))
+        }.store(in: &cancellables)
     }
 
     override func willResignActive() {
