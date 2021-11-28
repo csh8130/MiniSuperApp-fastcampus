d9a5aad50f1c911d489d1983569ece12de4a5c51
csh8130 <csh8130@naver.com>
2021-11-28 23:03:09 +0900

    카드 선택 영역 헤더 뷰 생성

--- MiniSuperApp/FinanceHome/CardOnFileDashboard/CardOnFileDashboardViewController.swift
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/CardOnFileDashboardViewController.swift
@@ -15,7 +15,62 @@ protocol CardOnFileDashboardPresentableListener: AnyObject {
 }
 
 final class CardOnFileDashboardViewController: UIViewController, CardOnFileDashboardPresentable, CardOnFileDashboardViewControllable {
-
+    
+    private let headerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "카드 및 계좌"
+        return label
+    }()
+    
+    private lazy var seeAllButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("전체보기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(seeAllButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.addSubview(headerStackView)
+        
+        headerStackView.addArrangedSubview(titleLabel)
+        headerStackView.addArrangedSubview(seeAllButton)
+        
+        NSLayoutConstraint.activate([
+            headerStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    @objc
+    private func seeAllButtonDidTap() {
+        
+    }
 
     weak var listener: CardOnFileDashboardPresentableListener?
 }
