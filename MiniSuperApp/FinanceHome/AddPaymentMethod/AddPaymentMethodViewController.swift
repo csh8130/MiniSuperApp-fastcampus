fa49b4adbd08fc63b59152060dd8c849ed27ffa0
csh8130 <csh8130@naver.com>
2021-12-03 02:35:56 +0900

    AddPaymentMethod 리블렛 생성

---
+++ MiniSuperApp/FinanceHome/AddPaymentMethod/AddPaymentMethodViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddPaymentMethodViewController.swift
+//  MiniSuperApp
+//
+//  Created by korit on 2021/12/03.
+//
+
+import ModernRIBs
+import UIKit
+
+protocol AddPaymentMethodPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class AddPaymentMethodViewController: UIViewController, AddPaymentMethodPresentable, AddPaymentMethodViewControllable {
+
+    weak var listener: AddPaymentMethodPresentableListener?
+}
