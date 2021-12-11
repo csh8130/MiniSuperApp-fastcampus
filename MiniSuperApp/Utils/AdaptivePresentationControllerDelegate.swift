a9ab04352f23aa1f1be2c78d0031a7436af8c91b
csh8130 <csh8130@naver.com>
2021-12-12 03:55:26 +0900

    드래그해서 닫기 처리를위해 DelegateProxy 생성

---
+++ MiniSuperApp/Utils/AdaptivePresentationControllerDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AdaptivePresentationControllerDelegate.swift
+//  MiniSuperApp
+//
+//  Created by korit on 2021/12/12.
+//
+
+import UIKit
+
+protocol AdaptivePresentationControllerDelegate: AnyObject {
+    func presentationControllerDidDismiss()
+}
+
+final class AdaptivePresentationControllerDelegateProxy: NSObject, UIAdaptivePresentationControllerDelegate {
+    weak var delegate: AdaptivePresentationControllerDelegate?
+    
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        delegate?.presentationControllerDidDismiss()
+    }
+}
