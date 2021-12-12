2d6de985b541f2efca524459ee32a080cff09856
csh8130 <csh8130@naver.com>
2021-12-12 16:29:30 +0900

    카드 추가 동작

---
+++ MiniSuperApp/FinanceHome/AddPaymentMethod/AddPaymentMethodInfo.swift
@@ -0,0 +1,14 @@
+//
+//  AddPaymentMethodInfo.swift
+//  MiniSuperApp
+//
+//  Created by korit on 2021/12/12.
+//
+
+import Foundation
+
+struct AddPaymentMethodInfo {
+    let number: String
+    let cvc: String
+    let expiration: String
+}
